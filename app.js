53a5ed0589404400b51d77ae079035d4fef69dff
JeanLucas00 <jeanlucas00@outlook.con.br>
2024-08-26 15:37:39 -0300

    Decodificador Pronto

---
+++ app.js
@@ -0,0 +1,32 @@
+function encryptText() {
+    const inputText = document.getElementById('inputText').value;
+    let encryptedText = inputText
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    
+    document.getElementById('outputText').value = encryptedText;
+}
+
+function decryptText() {
+    const inputText = document.getElementById('inputText').value;
+    let decryptedText = inputText
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    
+    document.getElementById('outputText').value = decryptedText;
+}
+
+function copyText() {
+    const textArea = document.getElementById('outputText');
+    
+    navigator.clipboard.writeText(textArea.value)
+        .then(() => {
+            alert('Texto copiado para a área de transferência!');
+        })
+}
